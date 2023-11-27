16cb0425a5b10380c36e1039cf0d5b9858816e43
Shaddy <shadybett540@gmail.com>
2023-11-27 14:39:21 +0300

    test complete

--- consonants.py
+++ consonants.py
@@ -1,20 +1,24 @@
-def words(letters):
+def solve(word):
+    vowels = ['a', 'e', 'i', 'o', 'u']
     value = []
-    vowels = ['a','e','i','o','u']
-    for letter in letters:
+    alphabet_values = {char: idx + 1 for idx, char in enumerate('abcdefghijklmnopqrstuvwxyz')}
+
+    current_consonant_value = 0
+    max_consonant_value = 0
+
+    for letter in word:
         if letter.lower() not in vowels:
             value.append(letter)
-        start_char = 'a'
-        end_char = 'z'
-        alphabet = [chr(char) for char in range(ord(start_char), ord(end_char) + 1)]  
-        
-        if value:
-            last_letter = value[-1]
-            index = alphabet.index(last_letter)
-            return index
+            current_consonant_value += alphabet_values[letter.lower()]
         else:
-            return None
-    
-result = words('strength')
-print(result)
+            # If a vowel is encountered, reset the current consonant value
+            current_consonant_value = 0
 
+        # Update the maximum consonant value
+        max_consonant_value = max(max_consonant_value, current_consonant_value)
+
+    return max_consonant_value
+
+# Example usage:
+result = solve('strength')
+print(result)

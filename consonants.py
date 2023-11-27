a0ba80fc11f4010de7bb5461f06bc5674c2c08be
Shaddy <shadybett540@gmail.com>
2023-11-27 14:28:50 +0300

    completed the last test

--- consonants.py
+++ consonants.py
@@ -4,7 +4,17 @@ def words(letters):
     for letter in letters:
         if letter.lower() not in vowels:
             value.append(letter)
-    return value
+        start_char = 'a'
+        end_char = 'z'
+        alphabet = [chr(char) for char in range(ord(start_char), ord(end_char) + 1)]  
+        
+        if value:
+            last_letter = value[-1]
+            index = alphabet.index(last_letter)
+            return index
+        else:
+            return None
     
-result = words('hello')
+result = words('wizard')
 print(result)
+

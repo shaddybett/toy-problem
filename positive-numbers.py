508fad1ca26037170958eea7e81fe9c2f952088c
Shaddy <shadybett540@gmail.com>
2023-11-27 12:36:01 +0300

    challenge 2 completed

--- positive-numbers.py
+++ positive-numbers.py
@@ -1,3 +1,14 @@
 def positive(a,b,c):
-    numbers = []
-    
+    numbers = [a,b,c]
+    positive_numbers = []
+    for number in numbers:
+        if number % 2 == 0 and number > 0:
+            positive_numbers.append(number)
+    pos_numbers = len(positive_numbers)       
+    if pos_numbers == 2:
+        return True
+    else:
+        return False
+
+result = positive(2,1,3)
+print(result)
